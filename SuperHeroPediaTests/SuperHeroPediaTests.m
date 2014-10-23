97a77596129fa44d648047227ae7827bcc034e30
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-23 17:38:30 -0500

    hackwich

--- SuperHeroPediaTests/SuperHeroPediaTests.m
+++ SuperHeroPediaTests/SuperHeroPediaTests.m
@@ -8,8 +8,12 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "SuperHero.h"
 
 @interface SuperHeroPediaTests : XCTestCase
+
+@property SuperHero *hero1;
+@property SuperHero *hero2;
 
 @end
 
@@ -17,12 +21,20 @@
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    NSDictionary *dic1 = @{@"name":@"Kevin", @"description":@"Really great guy!"};
+    NSDictionary *dic2 = @{@"name":@"Dave", @"description":@"Also an awesome guy"};
+
+    self.hero1 = [[SuperHero alloc]initWithDictionary:dic1];
+    self.hero2 = [[SuperHero alloc]initWithDictionary:dic2];
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+}
+
+-(void)testAllyStartsNotNil{
+    XCTAssert(self.hero1.allies != nil);
 }
 
 - (void)testExample {
